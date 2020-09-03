5c2b00585c75e803f7c43199d643f5796b3626f2
Andrew <lorandrewed@gmail.com>
2020-09-03 09:57:44 -0700

    add article index listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,8 @@ class ArticlesController < ApplicationController
       @article = Article.find(params[:id])  # '@' converts var to instance variable to access in the show view
                                             # params is whats passed into the url so /articles/1, 1 is the params
     end
+
+    def index
+      @articles = Article.all
+    end
 end
