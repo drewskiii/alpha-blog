9d42d74413671d90aeafd0c72b46362fe95e9e60
Andrew <lorandrewed@gmail.com>
2020-09-03 13:47:00 -0700

    create new article form with validation checks and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,21 @@ class ArticlesController < ApplicationController
     def index
       @articles = Article.all
     end
+
+    def new
+      @article = Article.new  # this allows the embedded ruby in new.html.erb to run since @article is has no errors and skips the conditional
+    end
+
+    def create
+      # render plain: params[:article] # shows the params in plaintext
+      @article = Article.new(params.require(:article).permit(:title, :description))  # permit title and description to create new article instance
+      # render plain: @article.inspect
+      if @article.save  # saves article in db, returns false if not saved, can access errors: article.errors.full_messages
+        flash[:notice] = "Article was created successfully."  # built-in ruby helper for flashes on certain actions
+        redirect_to @article # extract the id from the article and redirect to that article. Displayed in application.html.erb
+      else # if save fails due to things like validation errors
+        render 'new'  # renders the new.html.erb, then hits the error check in there and has access to this instance var @article
+      end
+                                          
+    end
 end
